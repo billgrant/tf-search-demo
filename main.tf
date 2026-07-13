7369e5c4828ecfe244b24ce52e3f651148d7acc6
Bill Grant <39649812+billgrant@users.noreply.github.com>
2026-07-13 14:28:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,384 @@ terraform {
     }
   }
 }
+
+
+import {
+  to       = aws_instance.unmanaged_0
+  provider = aws
+  identity = {
+    account_id = "769097531153"
+    id         = "i-09427fb067d52da04"
+    region     = "us-east-1"
+  }
+}
+resource "aws_instance" "unmanaged_0" {
+  provider                             = aws
+  ami                                  = "ami-0cfff439c82529f01"
+  associate_public_ip_address          = true
+  availability_zone                    = "us-east-1a"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = true
+  force_destroy                        = null
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t3.micro"
+  ipv6_address_count                   = 0
+  ipv6_addresses                       = []
+  key_name                             = "billgrant"
+  monitoring                           = false
+  placement_partition_number           = 0
+  private_ip                           = "172.31.20.183"
+  region                               = "us-east-1"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-1"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0d8c094e8aeb2225d"
+  tags = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-1"
+  }
+  tags_all = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-1"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0834b2f82853852d6"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 2
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 1
+    http_tokens                 = "optional"
+    instance_metadata_tags      = "disabled"
+  }
+  primary_network_interface {
+    network_interface_id = "eni-015995aef40f2e530"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+  timeouts {
+    create = null
+    delete = null
+    read   = null
+    update = null
+  }
+}
+
+import {
+  to       = aws_instance.unmanaged_1
+  provider = aws
+  identity = {
+    account_id = "769097531153"
+    id         = "i-056a403fe5576ceed"
+    region     = "us-east-1"
+  }
+}
+resource "aws_instance" "unmanaged_1" {
+  provider                             = aws
+  ami                                  = "ami-0cfff439c82529f01"
+  associate_public_ip_address          = true
+  availability_zone                    = "us-east-1a"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = true
+  force_destroy                        = null
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t3.micro"
+  ipv6_address_count                   = 0
+  ipv6_addresses                       = []
+  key_name                             = "billgrant"
+  monitoring                           = false
+  placement_partition_number           = 0
+  private_ip                           = "172.31.19.218"
+  region                               = "us-east-1"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-1"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0d8c094e8aeb2225d"
+  tags = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-2"
+  }
+  tags_all = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-2"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0834b2f82853852d6"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 2
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 1
+    http_tokens                 = "optional"
+    instance_metadata_tags      = "disabled"
+  }
+  primary_network_interface {
+    network_interface_id = "eni-0281723dea34fab25"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+  timeouts {
+    create = null
+    delete = null
+    read   = null
+    update = null
+  }
+}
+
+import {
+  to       = aws_instance.unmanaged_2
+  provider = aws
+  identity = {
+    account_id = "769097531153"
+    id         = "i-0caf11e7814d7ebc3"
+    region     = "us-east-1"
+  }
+}
+resource "aws_instance" "unmanaged_2" {
+  provider                             = aws
+  ami                                  = "ami-0cfff439c82529f01"
+  associate_public_ip_address          = true
+  availability_zone                    = "us-east-1a"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = true
+  force_destroy                        = null
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t3.micro"
+  ipv6_address_count                   = 0
+  ipv6_addresses                       = []
+  key_name                             = "billgrant"
+  monitoring                           = false
+  placement_partition_number           = 0
+  private_ip                           = "172.31.22.222"
+  region                               = "us-east-1"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-1"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0d8c094e8aeb2225d"
+  tags = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-3"
+  }
+  tags_all = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-3"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0834b2f82853852d6"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 2
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 1
+    http_tokens                 = "optional"
+    instance_metadata_tags      = "disabled"
+  }
+  primary_network_interface {
+    network_interface_id = "eni-09ac9586dbc3799df"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+  timeouts {
+    create = null
+    delete = null
+    read   = null
+    update = null
+  }
+}
+
+import {
+  to       = aws_instance.unmanaged_3
+  provider = aws
+  identity = {
+    account_id = "769097531153"
+    id         = "i-0d853ca3fdef9c058"
+    region     = "us-east-1"
+  }
+}
+resource "aws_instance" "unmanaged_3" {
+  provider                             = aws
+  ami                                  = "ami-0cfff439c82529f01"
+  associate_public_ip_address          = true
+  availability_zone                    = "us-east-1a"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = true
+  force_destroy                        = null
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t3.micro"
+  ipv6_address_count                   = 0
+  ipv6_addresses                       = []
+  key_name                             = "billgrant"
+  monitoring                           = false
+  placement_partition_number           = 0
+  private_ip                           = "172.31.16.62"
+  region                               = "us-east-1"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-1"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0d8c094e8aeb2225d"
+  tags = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-4"
+  }
+  tags_all = {
+    ManagedBy = "unmanaged"
+    Name      = "unmanaged-vm-4"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0834b2f82853852d6"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 2
+  }
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 1
+    http_tokens                 = "optional"
+    instance_metadata_tags      = "disabled"
+  }
+  primary_network_interface {
+    network_interface_id = "eni-0f53add6bf7fdec6a"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 10
+    volume_type           = "gp3"
+  }
+  timeouts {
+    create = null
+    delete = null
+    read   = null
+    update = null
+  }
+}
